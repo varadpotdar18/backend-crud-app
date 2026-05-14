d6f52b6c8494c26287e80828051433b3b659f1f2
Varad Potdar <varadpotdar184550@gmail.com>
2026-05-15 03:51:01 +0530

    prepared for vercel deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const userModel = require('./models/user');
+require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.json());
--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 const { name } = require('ejs');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test');
-
+mongoose.connect(process.env.MONGO_URI);
 const userSchema = new mongoose.Schema({
   image: String,
   email: String,
